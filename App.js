46213814569fc13c2c0698caf966feb7d13b9c3c
bp4sp4 <bp4sp4@naver.com>
2021-04-07 13:44:39 +0900

    테스트

---
+++ App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React, { Component } from 'react';
+ //import type {Node} from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
+ 
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+ 
+ class App extends Component{
+   constructor(){
+     super()
+     this.state = { loading: true, date: {}}
+   }
+   componentDidMount(){
+     //#simulate ajax call
+     setTimeout(() => {
+       this.setState({
+         loading: false,
+         data: {name: 'Nader Dabit', age:35}
+       })
+     }, 2000)
+   }
+   render(){
+     if(this.state.loading){
+       return <Text>Loading</Text>
+     }
+     const {name, age} = this.state.data
+     return(
+       <View>
+         <Text>Name: {name}</Text>
+         <Text>Age: {age}</Text>
+       </View>
+     )
+   }
+ }
+ export default App;
