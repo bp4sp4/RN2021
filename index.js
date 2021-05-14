bc75ceabe6a0581369a9f2c1ada09624f1a81418
bp4sp4 <bp4sp4@naver.com>
2021-05-14 14:07:24 +0900

    4-8 commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+import App from './app/App';
+import {name as ch4App2} from './app.json'
+const myComponentTest = () => <App />;
+AppRegistry.registerComponent(ch4App2,() => App);
